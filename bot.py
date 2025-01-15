03a7026ccc86266800a274a546a2bd8e6c8304ad
Hari-42 <harisaranmohanathas@hotmail.com>
2025-01-15 08:54:25 +0100

    base

---
+++ bot.py
@@ -0,0 +1,38 @@
+from abc import ABC, abstractmethod
+from typing import List, Optional
+
+from card import Card, CardType
+from game_handling.game_state import GameState
+
+
+class Bot(ABC):
+    def __init__(self, name: str):
+        self.alive: bool = True
+        self.name: str = name
+        self.hand: List[Card] = []
+
+    @abstractmethod
+    def play(self, state: GameState) -> Optional[Card]:
+        pass
+
+    @abstractmethod
+    def handle_exploding_kitten(self, state: GameState) -> int:
+        pass
+
+    @abstractmethod
+    def see_the_future(self, state: GameState, top_three: List[Card]):
+        pass
+
+    def add_card(self, card: Card):
+        self.hand.append(card)
+
+    def remove_card(self, card: Card):
+        self.hand.remove(card)
+
+    def has_defuse(self) -> bool:
+        return any(card.card_type == CardType.DEFUSE for card in self.hand)
+
+    def use_defuse(self) -> Card:
+        defuse = next(card for card in self.hand if card.card_type == CardType.DEFUSE)
+        self.remove_card(defuse)
+        return defuse
---
+++ main.py
@@ -0,0 +1,42 @@
+import argparse
+import copy
+
+from bot_loader import load_bots
+from card import CardCounts
+from game_handling.game import Game
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Exploding Kittens Lite")
+    parser.add_argument("--test", action="store_true", help="Run in test mode")
+
+    args = parser.parse_args()
+
+    bots = load_bots("bots")
+
+    if args.test:
+        # duplicate the first bot in the list x times so that we have multiple bots (deepcopy)
+        bots = [copy.deepcopy(bots[0]) for _ in range(4)]
+
+        for i in range(len(bots)):
+            bots[i].name = f"{bots[i].name}{i + 1}"
+
+    amount_of_players = len(bots)
+
+    card_counts = CardCounts(
+        EXPLODING_KITTEN=amount_of_players - 1,
+        DEFUSE=amount_of_players + int(amount_of_players / 2 + 0.5),
+        SKIP=amount_of_players + 6,
+        SEE_THE_FUTURE=amount_of_players * 2,
+        # ATTACK=amount_of_players,
+        NORMAL=amount_of_players * 6,
+    )
+
+    game = Game(args.test, bots, card_counts)
+    game.setup()
+    winner = game.play()
+    print(f"{winner.name} wins!")
+
+
+if __name__ == "__main__":
+    main()
